0ad5f42c3a8c05e42e25b60376ffb08f13306237
ruben <rubenvanstaden@gmail.com>
2021-10-21 14:19:00 -0400

    feat: add lualine plugin and setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,6 +65,35 @@ require("bufferline").setup {
     }
 }
 
+-- Lualine
+require'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'tokyonight',
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
 --
 -- Packer
 local fn = vim.fn
@@ -91,5 +120,10 @@ require('packer').startup(
     use 'folke/tokyonight.nvim'
 
     use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+
+    use {
+      'hoob3rt/lualine.nvim',
+      requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
   end
 )
