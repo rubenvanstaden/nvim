3786bd56f491ad00f4de336482fa83dac87cebf6
ruben <rubenvanstaden@gmail.com>
2021-10-21 09:48:27 -0400

    feat: add packer to init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,64 @@
-require('lua.core.mappings')
-require('lua.core.options')
+-- require('lua.plugin')
+-- require('lua.core')
+
+--
+-- Options
+local opt = vim.opt  -- to set options
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+
+opt.number = true         -- Display line numbers to the left
+opt.expandtab = true      -- Use spaces instead of tabs
+opt.shiftwidth = 2        -- Number of spaces tabs count for 
+opt.smartindent = true    -- Insert indents automatically
+opt.termguicolors = true  -- True color support
+opt.wrap = false          -- Disable line wrap
+
+-- 
+-- Mappings
+local function map(mode, bind, exec, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, bind, exec, opts)
+end
+
+local opt = {} --empty opt for maps with no extra options
+local M = {}
+
+-- Windows
+map("n", "<C-h>", "<C-w>h", opt)
+map("n", "<C-j>", "<C-w>j", opt)
+map("n", "<C-k>", "<C-w>k", opt)
+map("n", "<C-l>", "<C-w>l", opt)
+
+-- Dashboard 
+map("n", ";", ":", opt)
+map("n", "J", "10j", opt)
+map("n", "K", "10k", opt)
+
+--
+-- Packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
+  -- Plugins here
+  use 'wbthomason/packer.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+
+  use "junegunn/gv.vim"
+
+  use "tpope/vim-fugitive"
+
+end)
