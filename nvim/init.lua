7339c553c7bc7e4495926f4fec1f47f45a7aa28e
ruben <rubenvanstaden@gmail.com>
2021-10-22 11:33:52 -0400

    feat(compe): add tab completion keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -192,6 +192,59 @@ end
 local opt = {} --empty opt for maps with no extra options
 local M = {}
 
+-- compe tab-completion
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif vim.fn['vsnip#available'](1) == 1 then
+    return t "<Plug>(vsnip-expand-or-jump)"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+    return t "<Plug>(vsnip-jump-prev)"
+  else
+    -- If <S-Tab> is not working in your terminal, change it to <C-h>
+    return t "<S-Tab>"
+  end
+end
+
+-- function _G.completions()
+--     if vim.fn.pumvisible() == 1 then
+--       if vim.fn.complete_info()["selected"] ~= -1 then
+--         return vim.fn["compe#confirm"]("<CR>")
+--       end
+--     end
+-- end
+
+
+-- Compe mappings
+map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+-- map("i", "<CR>", "v:lua.completions()", {expr = true})
+map("i", "<CR>", "compe#confirm({ 'keys': '<CR>', 'select': v:true })", { expr = true })
+
 -- Windows
 map("n", "<C-h>", "<C-w>h", opt)
 map("n", "<C-j>", "<C-w>j", opt)
