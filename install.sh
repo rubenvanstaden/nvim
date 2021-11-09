ff0577dee25746db290a9e36fc968d8cd69d2f4c
Ruben van Staden <rubenvanstaden@gmail.com>
2021-11-09 15:02:34 -0500

    feat: add install script

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 set -ex
 
+rm -rf "$HOME/.local/share/nvim/site/pack/packer"
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+  "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+
 rm -rf ~/.config/nvim
 cp -R nvim ~/.config
