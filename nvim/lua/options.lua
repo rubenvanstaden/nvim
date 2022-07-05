f581c6dcec2b0349802986c4009888a9d2639af0
ruben <rubenvanstaden@gmail.com>
2022-07-05 10:24:27 -0400

    chore: make tab 4 spaces

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,7 +1,7 @@
 -- Locals
 vim.opt.number = true             -- Display line numbers to the left
 vim.opt.expandtab = true          -- Use spaces instead of tabs
-vim.opt.shiftwidth = 2            -- Number of spaces tabs count for 
+vim.opt.shiftwidth = 4            -- Number of spaces tabs count for 
 vim.opt.smartindent = true        -- Insert indents automatically
 vim.opt.termguicolors = true      -- True color support
 vim.opt.wrap = false              -- Disable line wrap
