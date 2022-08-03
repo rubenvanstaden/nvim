11998558f60518db31e93af19d7fb6c91c2f8c7d
ruben <rubenvanstaden@gmail.com>
2022-08-03 18:19:35 -0400

    chore: re-add snippet packages

--- nvim/lua/core/install.lua
+++ nvim/lua/core/install.lua
@@ -13,8 +13,8 @@ return packer.startup(
       'L3MON4D3/LuaSnip',
       'hrsh7th/nvim-cmp',
       'hrsh7th/cmp-nvim-lsp',
-      -- 'saadparwaiz1/cmp_luasnip',
-      -- "rafamadriz/friendly-snippets",
+      'saadparwaiz1/cmp_luasnip',
+      "rafamadriz/friendly-snippets",
     }
 
     -- Theme
--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -9,33 +9,33 @@ end
 local opt = {} --empty opt for maps with no extra options
 -- local M = {}
 
--- -- compe tab-completion
--- local t = function(str)
---   return vim.api.nvim_replace_termcodes(str, true, true, true)
--- end
---
--- local check_back_space = function()
---   local col = vim.fn.col('.') - 1
---   return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
--- end
+-- compe tab-completion
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
 
--- _G.tab_complete = function()
---   if vim.fn.pumvisible() == 1 then
---     return t "<C-n>"
---   elseif check_back_space() then
---     return t "<Tab>"
---   else
---     return vim.fn['compe#complete']()
---   end
--- end
---
--- _G.s_tab_complete = function()
---   if vim.fn.pumvisible() == 1 then
---     return t "<C-p>"
---   else
---     return t "<S-Tab>"
---   end
--- end
+local check_back_space = function()
+  local col = vim.fn.col('.') - 1
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  else
+    return t "<S-Tab>"
+  end
+end
 
 -- Keep visual mode when indenting
 map("v", "<", "<gv", opt)
