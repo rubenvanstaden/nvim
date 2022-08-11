21a30aeb909b42f75f8f43b8ee4d9327a593c994
ruben <rubenvanstaden@gmail.com>
2022-08-11 15:03:28 -0400

    fix: update packer install path

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 
 set -ex
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-  "$HOME/.config/local/share/nvim/site/pack/packer/start/packer.nvim"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 cp -R nvim ~/.config
 
