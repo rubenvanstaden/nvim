1c8e9c8f6359ede16d2a2b5a05a4ee98a0dee304
Ruben van Staden <rubenvanstaden@gmail.com>
2022-11-20 23:10:51 -0500

    chore: change deprecated setting

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local lsp = require("lspconfig")
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- https://github.com/rust-lang/rust-analyzer
 lsp.rust_analyzer.setup {
