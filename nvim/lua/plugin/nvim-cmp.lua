5dcf5deeb76e663a84255b6b619b105a0298b9e0
Ruben van Staden <rvs.cipher@proton.me>
2023-01-20 15:27:14 -0500

    refactor: remove tab completion with luasnip

--- nvim/lua/plugin/nvim-cmp.lua
+++ nvim/lua/plugin/nvim-cmp.lua
@@ -10,30 +10,8 @@ cmp.setup({
     },
 
     mapping = {
-
-        ['<C-Space>'] = cmp.mapping.complete(),
-
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-e>'] = cmp.mapping.close(),
-
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-
-        -- Toggles over all LuaSnip options.
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end,
     },
 
     sources = {
