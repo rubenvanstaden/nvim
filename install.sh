eb09518ad0f9a02bc8b56a4a5c63c4f724ab0c82
Ruben van Staden <rubenvanstaden@gmail.com>
2023-03-07 12:49:28 -0700

    chore: remove kanagawa theme and status line

--- install.sh
+++ install.sh
@@ -59,11 +59,9 @@ rm -rf cmp-nvim-lsp friendly-snippets cmp_luasnip
 # Themes
 
 git clone https://github.com/shaunsingh/nord.nvim.git
-git clone https://github.com/rebelot/kanagawa.nvim.git
 git clone https://github.com/cocopon/iceberg.vim.git
 git clone https://github.com/catppuccin/nvim.git
 git clone https://github.com/kadekillary/skull-vim.git
-git clone https://github.com/rebelot/heirline.nvim.git
 
 # Packages
 
