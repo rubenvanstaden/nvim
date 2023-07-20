73fe777559a103f50501be7984f9d6e84f197937
Ruben van Staden <rvs.cipher@proton.me>
2023-07-19 20:02:12 -0400

    remove NONE from color scheme

--- init.lua
+++ init.lua
@@ -114,7 +114,7 @@ local color = {
     nord0 = "#2E3440",
     nord1 = "#3B4252",
     nord2 = "#434C5E",
-    nord3 = "#4C566A",
+    nord3 = "#616E88", -- Out of pallette
     nord4 = "#D8DEE9",
     nord5 = "#E5E9F0",
     nord6 = "#ECEFF4",
@@ -127,30 +127,24 @@ local color = {
     nord13 = "#EBCB8B",
     nord14 = "#A3BE8C",
     nord15 = "#B48EAD",
-
-    -- Custom colors out of palette
-    none = "NONE",
-    nord3_comment = "#616E88",
-    nord4_onedark = "#ABB2BF",
+    nord16 = "#ABB2BF", -- Out of palette
 }
 
 -- Code
 vim.api.nvim_set_hl(0, "Type", { fg = color.nord9 })
 vim.api.nvim_set_hl(0, "Typedef", { link = "Type" })
-vim.api.nvim_set_hl(0, "Normal", { fg = color.nord4_onedark })
+vim.api.nvim_set_hl(0, "Normal", { fg = color.nord16 })
 vim.api.nvim_set_hl(0, "Statement", { fg = color.nord9 })
 vim.api.nvim_set_hl(0, "Structure", { fg = color.nord9 })
 vim.api.nvim_set_hl(0, "StorageClass", { fg = color.nord9 })
 vim.api.nvim_set_hl(0, "Boolean", { fg = color.nord9 })
 vim.api.nvim_set_hl(0, "Constant", { fg = color.nord7, bold = false })
-vim.api.nvim_set_hl(0, "Special", { fg = color.nord4_onedark, bold = true })
+vim.api.nvim_set_hl(0, "Special", { fg = color.nord16, bold = true })
 vim.api.nvim_set_hl(0, "Float", { fg = color.nord15 })
 vim.api.nvim_set_hl(0, "String", { fg = color.nord14 })
 vim.api.nvim_set_hl(0, "Number", { fg = color.nord15, italic = false })
 vim.api.nvim_set_hl(0, "Identifier", { fg = color.nord9, italic = true })
 vim.api.nvim_set_hl(0, "Function", { fg = color.nord8, italic = true })
---vim.api.nvim_set_hl(0, "Keyword", { fg = color.nord4_onedark, italic = false })
---vim.api.nvim_set_hl(0, "Conditional", { fg = color.nord4_onedark, italic = false })
 vim.api.nvim_set_hl(0, "Keyword", { fg = color.nord9 })
 vim.api.nvim_set_hl(0, "Conditional", { fg = color.nord9, bold = false })
 vim.api.nvim_set_hl(0, "Label", { fg = color.nord9 })
@@ -165,32 +159,32 @@ vim.api.nvim_set_hl(0, "SpecialChar", { fg = color.nord13 })
 vim.api.nvim_set_hl(0, "PreCondit", { fg = color.nord13 })
 vim.api.nvim_set_hl(0, "Character", { fg = color.nord14 })
 vim.api.nvim_set_hl(0, "Macro", { fg = color.nord9 })
-vim.api.nvim_set_hl(0, "Delimiter", { fg = color.nord4_onedark })
-vim.api.nvim_set_hl(0, "Comment", { fg = color.nord3_comment, italic = true })
+vim.api.nvim_set_hl(0, "Delimiter", { fg = color.nord16 })
+vim.api.nvim_set_hl(0, "Comment", { fg = color.nord3, italic = true })
 vim.api.nvim_set_hl(0, "SpecialComment", { fg = color.nord8 })
 vim.api.nvim_set_hl(0, "SpecialKey", { fg = color.nord9 })
-vim.api.nvim_set_hl(0, "MatchParen", { fg = color.nord15, bg = color.none, bold = true })
+vim.api.nvim_set_hl(0, "MatchParen", { fg = color.nord15, bold = true })
 vim.api.nvim_set_hl(0, "Todo", { fg = color.nord13 })
 vim.api.nvim_set_hl(0, "Search", { fg = color.nord6, bg = color.nord10 })
 
 -- Editor
-vim.api.nvim_set_hl(0, "Cursor", { fg = color.nord4, bg = color.none, reverse = true })
+vim.api.nvim_set_hl(0, "Cursor", { fg = color.nord4, reverse = true })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = color.nord1 })
 vim.api.nvim_set_hl(0, "StatusLine", { bg = color.nord2, fg = color.nord4 })
 vim.api.nvim_set_hl(0, "NonText", { fg = color.nord1 })
 vim.api.nvim_set_hl(0, "VertSplit", { fg = color.nord2 })
 vim.api.nvim_set_hl(0, "Title", { fg = color.nord14, bold = true })
-vim.api.nvim_set_hl(0, "Directory", { fg = color.nord7, bg = color.none })
-vim.api.nvim_set_hl(0, "Visual", { fg = color.none, bg = color.nord2 })
-vim.api.nvim_set_hl(0, "VisualMode", { fg = color.nord9, bg = color.none, reverse = true })
+vim.api.nvim_set_hl(0, "Directory", { fg = color.nord7 })
+vim.api.nvim_set_hl(0, "Visual", { bg = color.nord2 })
+vim.api.nvim_set_hl(0, "VisualMode", { fg = color.nord9, reverse = true })
 vim.api.nvim_set_hl(0, "ModeMsg", { fg = color.nord4 })
 vim.api.nvim_set_hl(0, "MoreMsg", { fg = color.nord4 })
 vim.api.nvim_set_hl(0, "Pmenu", { fg = color.nord4, bg = color.nord2 })
 vim.api.nvim_set_hl(0, "PmenuSel", { fg = color.nord4, bg = color.nord10 })
 vim.api.nvim_set_hl(0, "Substitute", { fg = color.nord0, bg = color.nord12 })
 vim.api.nvim_set_hl(0, "SignColumn", { fg = color.nord4, bg = color.nord0 })
-vim.api.nvim_set_hl(0, "LineNr", { bg = color.nonde, fg = color.nord4_onedark, bold = false })
-vim.api.nvim_set_hl(0, "CursorLineNr", { bg = color.none, fg = color.nord15, bold = true })
+vim.api.nvim_set_hl(0, "LineNr", { fg = color.nord16, bold = false })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = color.nord15, bold = true })
 
 -- Diagnostics
 vim.api.nvim_set_hl(0, "Error", { fg = color.nord11 })
@@ -201,12 +195,10 @@ vim.api.nvim_set_hl(0, "DiagnosticError", { link = "Error" })
 vim.api.nvim_set_hl(0, "DiagnosticWarn", { link = "Warnings" })
 
 -- Filetype
-vim.api.nvim_set_hl(0, "ledgerTransactionDate", { fg = color.nord4_onedark,  bold = true })
-vim.api.nvim_set_hl(0, "ledgerTransactionMetadata", { fg = color.nord3_comment,  bold = false })
-vim.api.nvim_set_hl(0, "shDerefSimple", { fg = color.nord4_onedark,  bold = true })
+vim.api.nvim_set_hl(0, "ledgerTransactionDate", { fg = color.nord16,  bold = true })
+vim.api.nvim_set_hl(0, "ledgerTransactionMetadata", { fg = color.nord3,  bold = false })
+vim.api.nvim_set_hl(0, "shDerefSimple", { fg = color.nord16,  bold = true })
 vim.api.nvim_set_hl(0, "markdownCode", { fg = color.nord7,  bold = false })
 vim.api.nvim_set_hl(0, "markdownItalic", { fg = color.nord8,  italic = true })
---vim.api.nvim_set_hl(0, "markdownH2", { fg = color.nord4_onedark,  bold = true })
---vim.api.nvim_set_hl(0, "goSingleDecl", { fg = color.nord8,  italic = true })
 
 -------------------------------------------------------------------------------
